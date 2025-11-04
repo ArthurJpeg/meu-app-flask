609bc13ed686ba287348d8a973e56c652bf5a150
ArthurJpeg <arthurmourajpg@gmail.com>
2025-11-04 19:15:23 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>🚀 Minha App Flask na Nuvem!</h1><p>Funcionando perfeitamente!</p>"
+
+@app.route("/sobre")
+def sobre():
+    return "<h1>Sobre</h1><p>Esta é uma aplicação de exemplo hospedada na nuvem!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
